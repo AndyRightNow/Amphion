1a680dc5b4619a6b0b5d063ab0a43e8b3c8f7759
AndyRightNow <naruto199507@gmail.com>
2025-01-06 22:11:46 +0900

    Refactor imports and update package configuration in pyproject.toml

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
-from models.tts.maskgct import *
+from models.tts import maskgct
 from pathlib import Path
+import utils
 
 maskgct_config_file = Path("models/tts/maskgct/config/maskgct.json").absolute()
 
